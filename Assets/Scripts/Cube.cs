89603b2836fc6a94fc33256158aadfa9a93d2b35
yogo96 <yogomna96@mail.ru>
2024-10-30 13:27:07 +0300

    some changes

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+[RequireComponent(typeof(MeshRenderer))]
 public class Cube : MonoBehaviour
 {
     [SerializeField] private int _lifeTimeMin = 2;
@@ -10,42 +11,50 @@ public class Cube : MonoBehaviour
 
     private Spawner _spawner;
     private Material _material;
-    private bool _isTouched = false;
-    private WaitForSeconds _waitForSeconds = new WaitForSeconds(1);
+    private bool _isTouchedPlatform;
+    private WaitForSeconds _waitTime;
 
     private void Awake()
     {
-        _spawner = FindObjectOfType<Spawner>();
-        _material = gameObject.GetComponent<MeshRenderer>().material;
+        _isTouchedPlatform = false;
+        _waitTime = new WaitForSeconds(1);
+        _material = GetComponent<MeshRenderer>().material;
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (_isTouched == true)
+        if (_isTouchedPlatform == true)
             return;
 
         if (other.gameObject.TryGetComponent<Platform>(out Platform _) == false)
             return;
 
-        _isTouched = true;
+        _isTouchedPlatform = true;
         _material.color = Random.ColorHSV();
-        StartCoroutine(LifeTiming(Random.Range(_lifeTimeMin, _lifeTimeMax + 1)));
+        StartCoroutine(WaitToDestroying(Random.Range(_lifeTimeMin, _lifeTimeMax + 1)));
     }
 
-    private void ResetState()
+    public void SetSpawner(Spawner spawner)
     {
-        _isTouched = false;
-        _material.color = _baseColor;
+        _spawner = spawner;
     }
 
-    private IEnumerator LifeTiming(int lifeTime)
+    public void ResetCube(Vector3 startPosition)
+    {
+        _isTouchedPlatform = false;
+        _material.color = _baseColor;
+        transform.position = startPosition;
+        transform.rotation = Quaternion.identity;
+        enabled = true;
+    }
+
+    private IEnumerator WaitToDestroying(int lifeTime)
     {
         for (int i = 0; i < lifeTime; i++)
         {
-            yield return _waitForSeconds;
+            yield return _waitTime;
         }
 
         _spawner.DestroyCube(this);
-        ResetState();
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,23 +6,17 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private BoxCollider _platform;
     [SerializeField] private Cube _cubePrefab;
-    [SerializeField] private float _startPositionY = 25;
+    [SerializeField] private float _spawnPositionY = 25;
     [SerializeField] private int _maxCubeCount = 10;
 
     private ObjectPool<Cube> _objectPool;
-
-    private void Update()
-    {
-        if (_objectPool.CountActive < _maxCubeCount)
-            _objectPool.Get();
-    }
 
     public void DestroyCube(Cube cube)
     {
         _objectPool.Release(cube);
     }
-
-    private void Start()
+    
+    private void Awake()
     {
         _objectPool = new ObjectPool<Cube>(
             CreateCube,
@@ -35,23 +29,28 @@ public class Spawner : MonoBehaviour
         );
     }
 
+    private void Update()
+    {
+        if (_objectPool.CountActive < _maxCubeCount)
+            _objectPool.Get();
+    }
+
     private void ActionOnGet(Cube cube)
     {
         Vector3 boundsMin = _platform.bounds.min;
         Vector3 boundsMax = _platform.bounds.max;
         Vector3 startPosition = new Vector3(
             Random.Range(boundsMin.x, boundsMax.x),
-            _startPositionY,
+            _spawnPositionY,
             Random.Range(boundsMin.z, boundsMax.z)
         );
-        cube.transform.position = startPosition;
-        cube.transform.rotation = Quaternion.identity;
-        cube.enabled = true;
+        cube.ResetCube(startPosition);
     }
 
     private Cube CreateCube()
     {
         Cube cube = Instantiate(_cubePrefab);
+        cube.SetSpawner(this);
         cube.enabled = false;
         return cube;
     }
